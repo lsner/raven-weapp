66592cc3774f0941c534d28eea8f947014aaede2
lsner <740501011@qq.com>
2020-04-09 18:51:36 +0800

    console 改回来 编译

--- build/raven.js
+++ build/raven.js
@@ -11,7 +11,7 @@ RavenConfigError.prototype.constructor = RavenConfigError;
 module.exports = RavenConfigError;
 
 },{}],2:[function(require,module,exports){
-'use strict';
+"use strict";
 
 var wrapMethod = function(console, level, callback) {
   var originalConsoleLevel = console[level];
@@ -21,25 +21,30 @@ var wrapMethod = function(console, level, callback) {
     return;
   }
 
-  var sentryLevel = level === 'warn' ? 'warning' : level;
+  var sentryLevel = level === "warn" ? "warning" : level;
 
   console[level] = function() {
     var args = [].slice.call(arguments);
 
-    // var msg = '' + args.join(' ');
-    var msg = '' + args.map(function(arg) {
-      if (typeof arg === 'object') {
-          return JSON.stringify(arg);
-      }
-      return arg;
-    }).join(' ');
-    
-    var data = {level: sentryLevel, logger: 'console', extra: {arguments: args}};
+    var msg = "" + args.join(" ");
+    // var msg = '' + args.map(function(arg) {
+    //   if (typeof arg === 'object') {
+    //       return JSON.stringify(arg);
+    //   }
+    //   return arg;
+    // }).join(' ');
 
-    if (level === 'assert') {
+    var data = {
+      level: sentryLevel,
+      logger: "console",
+      extra: { arguments: args }
+    };
+
+    if (level === "assert") {
       if (args[0] === false) {
         // Default browsers message
-        msg = 'Assertion failed: ' + (args.slice(1).join(' ') || 'console.assert');
+        msg =
+          "Assertion failed: " + (args.slice(1).join(" ") || "console.assert");
         data.extra.arguments = args.slice(1);
         callback && callback(msg, data);
       }
